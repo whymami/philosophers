2e99176cfcd4497fac91e5a07d74079a3a0a2882
whymami <mmg165316@gmail.com>
2024-05-26 18:49:35 +0300

    leaks kontrol

--- main.c
+++ main.c
@@ -6,11 +6,41 @@
 /*   By: muguveli <muguveli@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 20:31:40 by muguveli          #+#    #+#             */
-/*   Updated: 2024/05/25 17:04:15 by muguveli         ###   ########.fr       */
+/*   Updated: 2024/05/26 18:48:44 by muguveli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void dispose(t_philo *philo, pthread_mutex_t *forks, int count) 
+{
+    int i;
+
+    // Mutex'leri yok etme
+    for (i = 0; i < philo->data->philo_count; i++) {
+        pthread_mutex_destroy(philo[i].last_eat_mutex);
+        if (philo[i].data->must_eat > 0)
+            pthread_mutex_destroy(philo[i].loop_count_mutex);
+    }
+
+    pthread_mutex_destroy(philo->data->display);
+    for (i = 0; i < philo->data->philo_count; i++) {
+        pthread_mutex_destroy(philo[i].left_fork);
+        pthread_mutex_destroy(philo[i].right_fork);
+    }
+
+    free(philo->data->display);
+    for (i = 0; i < count; i++) 
+	{
+        free(philo[i].last_eat_mutex);
+        if (philo[i].data->must_eat > 0)
+            free(philo[i].loop_count_mutex);
+    }
+    free(philo->data);
+    free(philo);
+    free(forks);
+	system("leaks philo");
+}
 
 void	ft_usleep(int time)
 {
@@ -60,5 +90,5 @@ int	main(int argc, char **argv)
 	if (!init_thread(philo))
 		return (1);
 	if (stalker(philo))
-		return (1);
+		dispose(philo, forks, philo->data->philo_count);
 }
